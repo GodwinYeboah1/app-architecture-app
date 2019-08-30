2e0559156537905de7db248aee98cad4fcfdd50e
Godwin Yeboah <gyeboah@naccho.org>
2019-08-30 14:13:55 -0400

    app architecture video follow along

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks: any[] = [
+    {title:'first task'},
+    {title:'second task'},
+    {title:'third task'}
+    ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
